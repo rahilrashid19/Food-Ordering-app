961650b685613813244d75bf068ee3ebd98e89a6
Rahil Rashid <rrashid@sparkcognition.com>
2023-12-28 14:26:32 +0530

    change

--- src/components/ResturantDetails.js
+++ src/components/ResturantDetails.js
@@ -7,8 +7,8 @@ import { addItems, removeItems } from "../utils/cartSlice";
 
 const RestaurantDetails = () => {
   const { id } = useParams();
-
   const dispatch = useDispatch();
+  const restaurantData = useResturauntHook(id);
 
   const addItemsToCart = (dish) => {
     dispatch(addItems(dish));
@@ -18,68 +18,81 @@ const RestaurantDetails = () => {
     dispatch(removeItems(dish));
   };
 
-  const restaurantData = useResturauntHook(id);
+  return (
+    <>
+      {restaurantData.length === 0 ? (
+        <Shimmer />
+      ) : (
+        <div className="flex flex-col md:flex-row gap-6 h-[76vh]">
+          <img
+            src={
+              CDN_IMG_URL +
+              restaurantData?.data?.cards[0]?.card?.card?.info
+                ?.cloudinaryImageId
+            }
+            alt=""
+            className="h-80"
+          />
+          <div className="p-4 md:w-1/2">
+            <h1 className="text-2xl font-bold mb-2">
+              {restaurantData?.data?.cards[0]?.card?.card?.info?.name}
+            </h1>
+            <p className="text-gray-600 mb-2">
+              {
+                restaurantData?.data?.cards[0]?.card?.card?.info
+                  ?.costForTwoMessage
+              }
+            </p>
+            <p className="text-gray-600 mb-2">
+              {restaurantData?.data?.cards[0]?.card?.card?.info?.areaName}
+            </p>
+            <p className="text-yellow-500 mb-2">
+              {restaurantData?.data?.cards[0]?.card?.card?.info?.avgRating}{" "}
+              stars
+            </p>
+            <p className="text-gray-600">
+              {restaurantData?.data?.cards[0]?.card?.card?.info?.city}
+            </p>
 
-  return restaurantData.length === 0 ? (
-    <Shimmer />
-  ) : (
-    <div className="flex flex-col md:flex-row gap-6 h-[76vh]">
-      <img
-        src={
-          CDN_IMG_URL +
-          restaurantData?.data?.cards[0]?.card?.card?.info?.cloudinaryImageId
-        }
-        alt=""
-        className="h-80"
-      />
-      <div className="p-4 md:w-1/2">
-        <h1 className="text-2xl font-bold mb-2">
-          {restaurantData?.data?.cards[0]?.card?.card?.info?.name}
-        </h1>
-        <p className="text-gray-600 mb-2">
-          {restaurantData?.data?.cards[0]?.card?.card?.info?.costForTwoMessage}
-        </p>
-        <p className="text-gray-600 mb-2">
-          {restaurantData?.data?.cards[0]?.card?.card?.info?.areaName}
-        </p>
-        <p className="text-yellow-500 mb-2">
-          {restaurantData?.data?.cards[0]?.card?.card?.info?.avgRating} stars
-        </p>
-        <p className="text-gray-600">
-          {restaurantData?.data?.cards[0]?.card?.card?.info?.city}
-        </p>
-        <ul className="space-y-4 border rounded-lg shadow-md p-4">
-          {restaurantData.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1]?.card?.card?.carousel?.map(
-            (card) => (
-              <li
-                key={card.dish.info.id}
-                className="flex items-center justify-between"
-              >
-                <span>{card.dish.info.name}</span>
-                <div className="flex content-between">
-                  <button
-                    className="bg-green-500 text-white px-2 py-1 rounded"
-                    onClick={() => {
-                      addItemsToCart(card);
-                    }}
-                  >
-                    Add to Cart
-                  </button>
-                  <button
-                    className="bg-red-500 text-white px-2 py-1 rounded"
-                    onClick={() => {
-                      removeItemsFromCart(card);
-                    }}
-                  >
-                    Remove From Cart
-                  </button>
-                </div>
-              </li>
-            )
-          )}
-        </ul>
-      </div>
-    </div>
+            {restaurantData.data.cards[2].groupedCard.cardGroupMap.REGULAR
+              .cards[1]?.card?.card?.carousel?.length > 0 ? (
+              <ul className="space-y-4 border rounded-lg shadow-md p-4">
+                {restaurantData.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1]?.card?.card?.carousel?.map(
+                  (card) => (
+                    <li
+                      key={card.dish.info.id}
+                      className="flex items-center justify-between gap-6"
+                    >
+                      <span>{card.dish.info.name}</span>
+                      <div className="flex content-between">
+                        <button
+                          className="bg-green-500 text-white px-2 py-1 rounded"
+                          onClick={() => {
+                            addItemsToCart(card);
+                          }}
+                        >
+                          Add to Cart
+                        </button>
+                        <button
+                          className="bg-red-500 text-white px-2 py-1 rounded"
+                          onClick={() => {
+                            removeItemsFromCart(card);
+                          }}
+                        >
+                          Remove From Cart
+                        </button>
+                      </div>
+                    </li>
+                  )
+                )}
+              </ul>
+            ) : (
+              <p className="text-gray-600 mt-4">No menu available.</p>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
